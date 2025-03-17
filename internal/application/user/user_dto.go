c391ea1b1453017c5592d4b961a530dfe0d32236
Tarique Iqbal <live.iqbal@gmail.com>
2025-03-17 02:57:03 +0100

    Add user dto

---
+++ internal/application/user/user_dto.go
@@ -0,0 +1,16 @@
+package user
+
+type UserCreateDTO struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,min=6"`
+}
+
+type UserResponseDTO struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Role      string `json:"role"`
+}
