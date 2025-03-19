a72128f82a54897e8f6f941f8cbdff279eca6859
Tarique Iqbal <live.iqbal@gmail.com>
2025-03-19 02:04:21 +0100

    Add postgres db connection

---
+++ internal/infrastructure/db/postgres.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
