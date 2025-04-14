0698322d9e5e28726467e5575899b96132ed6552
Tarique Iqbal <live.iqbal@gmail.com>
2025-04-14 07:50:39 +0200

    Add main.go file

---
+++ email-service/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"email-service/internal/container"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app, err := container.NewContainer()
+	if err != nil {
+		log.Fatalf("Failed to initialize container: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go app.Consumer.Run(ctx, app.Consumer.GetMessages(), app.Dispatcher)
+
+	<-sigs
+	log.Println("Exiting...")
+	cancel()
+	app.Consumer.Close()
+}
