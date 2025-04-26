ff18b7c6ed79e392c78b7bfa9ff2ae868936fd3a
Tarique Iqbal <live.iqbal@gmail.com>
2025-04-26 14:24:28 +0200

    Fix fields in restaurant struct

--- api-service/internal/domain/restaurant/restaurant.go
+++ api-service/internal/domain/restaurant/restaurant.go
@@ -3,11 +3,11 @@ package restaurant
 import "time"
 
 type Restaurant struct {
-	ID        uint       `json:"id" gorm:"primaryKey"`
-	UserID    uint       `json:"user_id" gorm:"not null"`
-	Name      string     `json:"name" gorm:"not null"`
-	Slug      string     `json:"slug" gorm:"unique;not null"`
-	Address   string     `json:"address" gorm:"not null"`
-	CreatedAt time.Time  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt *time.Time `gorm:"default:null"`
+	ID        uint       `gorm:"primaryKey"`
+	UserID    uint       `gorm:"not null"`
+	Name      string     `gorm:"size:255;not null"`
+	Slug      string     `gorm:"size:255;unique;not null"`
+	Address   string     `gorm:"size:511;not null"`
+	CreatedAt time.Time  `gorm:"autoCreateTime"`
+	UpdatedAt *time.Time `gorm:"autoUpdateTime;default:null"`
 }
