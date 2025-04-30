2c14fa11b6326ea03720d0a9424a1ee950cb59a1
Tarique Iqbal <live.iqbal@gmail.com>
2025-04-30 20:09:06 +0200

    Add pizza size dto

---
+++ api-service/internal/application/restaurant/pizza_size_dto.go
@@ -0,0 +1,14 @@
+package restaurant
+
+type PizzaSizeCreateDTO struct {
+	Title string `json:"title" binding:"required"`
+	Size  int    `json:"size" binding:"required,gt=0"`
+}
+
+type PizzaSizeResponseDTO struct {
+	ID        uint    `json:"id"`
+	Title     string  `json:"title"`
+	Size      int     `json:"size"`
+	CreatedAt string  `json:"created_at"`
+	UpdatedAt *string `json:"updated_at,omitempty"`
+}
