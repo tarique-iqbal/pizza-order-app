9b0c733c0a0b5df885d987f0d70278373641c5c6
Tarique Iqbal <live.iqbal@gmail.com>
2025-05-16 21:24:11 +0200

    chore: remove outdated database migrations

--- api-service/internal/infrastructure/migrations/010_alter_restaurant_addresses.down.sql
+++
@@ -1,5 +0,0 @@
--- Step 1: Drop the GIN index
-DROP INDEX IF EXISTS idx_restaurant_addresses_full_text;
-
--- Step 2: Drop the column
-ALTER TABLE restaurant_addresses DROP COLUMN IF EXISTS full_text;
--- api-service/internal/infrastructure/migrations/010_alter_restaurant_addresses.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE restaurant_addresses ADD COLUMN full_text VARCHAR(511) NOT NULL;
-
-CREATE INDEX idx_restaurant_addresses_full_text 
-ON restaurant_addresses 
-USING GIN (to_tsvector('german', full_text));
