5aec678adb7f8c688871ac86d4218afbdf9d345c
Tarique Iqbal <live.iqbal@gmail.com>
2025-05-21 13:00:35 +0200

    feat: add `restaurant-address` entity

---
+++ api-service/internal/domain/restaurant/restaurant_address.go
@@ -0,0 +1,14 @@
+package restaurant
+
+type RestaurantAddress struct {
+	ID         uint   `gorm:"primaryKey"`
+	House      string `gorm:"size:63;not null"`
+	Street     string `gorm:"size:127;not null"`
+	City       string `gorm:"size:63;not null"`
+	PostalCode string `gorm:"size:10;not null"`
+	FullText   string `gorm:"type:text"`
+}
+
+func (RestaurantAddress) TableName() string {
+	return "restaurant_addresses"
+}
