72e230adecce375aaf55378e53d7413b7b4b84d1
Tarique Iqbal <live.iqbal@gmail.com>
2025-06-16 12:45:57 +0200

    fix: correct user role permission in middleware

--- api-service/internal/interfaces/http/middlewares/middleware.go
+++ api-service/internal/interfaces/http/middlewares/middleware.go
@@ -14,6 +14,6 @@ type Middleware struct {
 func NewMiddleware(jwt auth.JWTService) *Middleware {
 	return &Middleware{
 		Auth:        AuthMiddleware(jwt),
-		EnsureOwner: RequireRole("Owner"),
+		EnsureOwner: RequireRole("owner"),
 	}
 }
