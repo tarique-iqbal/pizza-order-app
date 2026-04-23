7821a3ea008df8540a89af77328e8a67b363898f
Tarique Iqbal <live.iqbal@gmail.com>
2026-04-23 16:24:08 +0200

    refactor: remove unused user domain code

--- restaurant-service/internal/domain/user/user_repository.go
+++
@@ -1,9 +0,0 @@
-package user
-
-import "context"
-
-type UserRepository interface {
-	Create(ctx context.Context, user *User) error
-	FindByEmail(ctx context.Context, email string) (*User, error)
-	EmailExists(email string) (bool, error)
-}
