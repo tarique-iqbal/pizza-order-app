11a0cdcbf106d4f88e2bcbc303ce7ac5b676cf8e
Tarique Iqbal <live.iqbal@gmail.com>
2026-05-04 22:12:06 +0200

    refactor(restaurant): update restaurant domain model

--- restaurant-service/internal/domain/restaurant/restaurant.go
+++ restaurant-service/internal/domain/restaurant/restaurant.go
@@ -4,23 +4,43 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
+)
+
+type DeliveryType string
+type RestaurantStatus string
+
+const (
+	DeliveryOwn        DeliveryType = "own"
+	DeliveryThirdParty DeliveryType = "third_party"
+	DeliveryNone       DeliveryType = "none"
+)
+
+const (
+	StatusDraft    RestaurantStatus = "draft"
+	StatusReview   RestaurantStatus = "review"
+	StatusActive   RestaurantStatus = "active"
+	StatusInactive RestaurantStatus = "inactive"
+	StatusDisabled RestaurantStatus = "disabled"
+	StatusRejected RestaurantStatus = "rejected"
 )
 
 type Restaurant struct {
-	ID             uint              `gorm:"primaryKey"`
-	RestaurantUUID uuid.UUID         `gorm:"type:uuid;not null;uniqueIndex"`
-	UserID         uint              `gorm:"not null;index"`
-	Name           string            `gorm:"size:200;not null"`
-	Slug           string            `gorm:"size:255;uniqueIndex;not null"`
-	Email          string            `gorm:"size:255;uniqueIndex;not null"`
-	Phone          string            `gorm:"size:32;not null"`
-	AddressID      uint              `gorm:"not null"`
-	Address        RestaurantAddress `gorm:"foreignKey:AddressID"`
-	DeliveryType   string            `gorm:"size:32;not null"`
-	DeliveryKm     int               `gorm:"not null;"`
-	Specialties    string            `gorm:"type:text"`
-	CreatedAt      time.Time         `gorm:"autoCreateTime"`
-	UpdatedAt      *time.Time        `gorm:"autoUpdateTime;default:null"`
+	ID           uuid.UUID        `gorm:"type:uuid;primaryKey"`
+	OwnerID      uuid.UUID        `gorm:"type:uuid;not null;index"`
+	Name         string           `gorm:"size:128;not null"`
+	VATNumber    string           `gorm:"size:16;not null"`
+	Slug         *string          `gorm:"size:255;uniqueIndex"`
+	Email        *string          `gorm:"size:255;uniqueIndex"`
+	Phone        *string          `gorm:"size:32"`
+	Pickup       bool             `gorm:"not null;default:true"`
+	DeliveryKm   *int16           `gorm:"check:delivery_km BETWEEN 1 AND 25"`
+	DeliveryType DeliveryType     `gorm:"type:restaurant_delivery_type_enum;not null;default:'none'"`
+	Specialties  *string          `gorm:"size:255"`
+	Checklist    datatypes.JSON   `gorm:"type:jsonb;not null"`
+	Status       RestaurantStatus `gorm:"type:restaurant_status_enum;not null;default:'draft'"`
+	CreatedAt    time.Time        `gorm:"autoCreateTime"`
+	UpdatedAt    *time.Time       `gorm:"autoUpdateTime"`
 }
 
 func (Restaurant) TableName() string {
